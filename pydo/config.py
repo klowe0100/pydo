cdec8c437bb402b456e0b19e65cc56bdbfb00259
Lyz <lyz@riseup.net>
2020-08-29 14:07:46 +0200

    refactor: add type hints and fixtures to the configuration tests

--- pydo/config.py
+++ pydo/config.py
@@ -5,13 +5,12 @@ Classes:
     Config: Class to manipulate the configuration of the program.
 """
 
-from collections import UserDict
-from ruamel.yaml import YAML
-from ruamel.yaml.scanner import ScannerError
-
 import logging
 import os
-import sys
+from collections import UserDict
+from typing import Any, Dict, List, Union
+
+from ruamel.yaml import YAML
 
 log = logging.getLogger(__name__)
 
@@ -39,7 +38,7 @@ class Config(UserDict):
         self.config_path = os.path.expanduser(config_path)
         self.load()
 
-    def get(self, key):
+    def get(self, key: str, default: Any = None) -> Union[str, int, Dict, List]:
         """
         Fetch the configuration value of the specified key. If there are nested
         dictionaries, a dot notation can be used.
@@ -53,10 +52,8 @@ class Config(UserDict):
         }
 
         self.data.get('first.second') == 'value'
-
-        Arguments:
-            key(str): Configuration key to fetch
         """
+
         keys = key.split(".")
         value = self.data.copy()
 
@@ -65,24 +62,36 @@ class Config(UserDict):
 
         return value
 
-    def load(self):
+    def set(self, key: str, value: Union[str, int]) -> None:
+        """
+        Set the configuration value of the specified key. If there are nested
+        dictionaries, a dot notation can be used.
+
+        So if you want to set the configuration:
+
+        self.data = {
+            'first': {
+                'second': 'value'
+            },
+        }
+
+        self.data.set('first.second', 'value')
+        """
+        keys: List = key.split(".")
+
+        parent = self.get(".".join(keys[:-1]))
+        if isinstance(parent, dict):
+            parent[keys[-1]] = value
+        else:
+            raise ValueError("No configuration is found under the path {key}")
+
+    def load(self) -> None:
         """
         Loads configuration from configuration YAML file.
         """
 
-        try:
-            with open(os.path.expanduser(self.config_path), "r") as f:
-                try:
-                    self.data = YAML().load(f)
-                except ScannerError as e:
-                    log.error(
-                        "Error parsing yaml of configuration file "
-                        "{}: {}".format(e.problem_mark, e.problem,)
-                    )
-                    sys.exit(1)
-        except FileNotFoundError:
-            log.error("Error opening configuration file {}".format(self.config_path))
-            sys.exit(1)
+        with open(os.path.expanduser(self.config_path), "r") as f:
+            self.data = YAML().load(f)
 
     def save(self):
         """
--- tests/unit/test_configuration.py
+++ tests/unit/test_configuration.py
@@ -1,13 +1,3 @@
-from pydo.configuration import Config
-from unittest.mock import patch
-from ruamel.yaml.scanner import ScannerError
-
-import os
-import pytest
-import shutil
-import tempfile
-
-
 class TestConfig:
     """
     Class to test the Config object.
@@ -16,73 +6,32 @@ class TestConfig:
         config (Config object): Config object to test
     """
 
-    @pytest.fixture(autouse=True)
-    def setup(self):
-        self.config_path = "assets/config.yaml"
-        self.log_patch = patch("pydo.configuration.log", autospect=True)
-        self.log = self.log_patch.start()
-        self.sys_patch = patch("pydo.configuration.sys", autospect=True)
-        self.sys = self.sys_patch.start()
-
-        self.config = Config(self.config_path)
-        yield "setup"
-
-        self.log_patch.stop()
-        self.sys_patch.stop()
-
-    def test_get_can_fetch_nested_items_with_dots(self):
-        self.config.data = {
+    def test_get_can_fetch_nested_items_with_dots(self, config):
+        config.data = {
             "first": {"second": "value"},
         }
 
-        assert self.config.get("first.second") == "value"
+        assert config.get("first.second") == "value"
 
-    def test_config_can_fetch_nested_items_with_dictionary_notation(self):
-        self.config.data = {
+    def test_set_can_set_nested_items_with_dots(self, config):
+        config.set("storage.type", "tinydb")
+        assert config.data["storage"]["type"] == "tinydb"
+
+    def test_config_can_fetch_nested_items_with_dictionary_notation(self, config):
+        config.data = {
             "first": {"second": "value"},
         }
 
-        assert self.config["first"]["second"] == "value"
+        assert config["first"]["second"] == "value"
 
-    def test_config_load(self):
-        self.config.load()
-        assert len(self.config.data["task"]) > 0
+    def test_config_load(self, config):
+        config.load()
+        assert len(config.data["task"]) > 0
 
-    @patch("pydo.configuration.YAML")
-    def test_load_handles_wrong_file_format(self, yamlMock):
-        yamlMock.return_value.load.side_effect = ScannerError(
-            "error", "", "problem", "mark",
-        )
+    def test_save_config(self, config):
+        config.data = {"a": "b"}
 
-        self.config.load()
-        self.log.error.assert_called_once_with(
-            "Error parsing yaml of configuration file mark: problem"
-        )
-        self.sys.exit.assert_called_once_with(1)
+        config.save()
 
-    @patch("pydo.configuration.open")
-    def test_load_handles_file_not_found(self, openMock):
-        openMock.side_effect = FileNotFoundError()
-
-        self.config.load()
-        self.log.error.assert_called_once_with(
-            "Error opening configuration file {}".format(self.config_path)
-        )
-        self.sys.exit.assert_called_once_with(1)
-
-    @patch("pydo.configuration.Config.load")
-    def test_init_calls_config_load(self, loadMock):
-        Config()
-        loadMock.assert_called_once_with()
-
-    def test_save_config(self):
-        tmp = tempfile.mkdtemp()
-        save_file = os.path.join(tmp, "yaml_save_test.yaml")
-        self.config = Config(save_file)
-        self.config.data = {"a": "b"}
-
-        self.config.save()
-        with open(save_file, "r") as f:
+        with open(config.config_path, "r") as f:
             assert "a:" in f.read()
-
-        shutil.rmtree(tmp)
