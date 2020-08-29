6a9445913b30901361fc382d212b0032f08da5d8
Lyz <lyz@riseup.net>
2020-08-29 14:13:44 +0200

    refactor: add manager into services

--- pydo/services.py
+++ pydo/services.py
@@ -6,18 +6,122 @@ Functions:
     export: Function to export the database to json to stdout.
 """
 
-# import json
+import logging
 import os
+from typing import Dict, List, Tuple
 
 import alembic.config
 
-# from pydo.model import engine
+from pydo import model
+from pydo.adapters import repository
 
-# from sqlalchemy import MetaData
+log = logging.getLogger(__name__)
+
+
+def _configure_task(repo: repository.AbstractRepository, task: model.Task) -> None:
+    """
+    Internal Function to configure the new task:
+        * Setting task project
+        * Setting task tags
+    """
+    set_task_project(repo, task)
+    set_task_tags(repo, task)
+
+
+def add_task(repo: repository.AbstractRepository, task_attributes: Dict) -> model.Task:
+    """
+    Function to create a new task.
+    """
+
+    task = model.Task(repo.create_next_fulid(model.Task), **task_attributes)
+    _configure_task(repo, task)
+
+    repo.add(task)
+    repo.commit()
+    log.info(f"Added task {task.id}: {task.description}")
+
+    return task
+
+
+def add_recurrent_task(
+    repo: repository.AbstractRepository, task_attributes: Dict
+) -> Tuple[model.RecurrentTask, model.Task]:
+    """
+    Function to create a new recurring or repeating task.
+
+    Returns both parent and first children tasks.
+    """
+
+    parent_task = model.RecurrentTask(
+        repo.create_next_fulid(model.Task), **task_attributes
+    )
+    _configure_task(repo, parent_task)
+
+    task_attributes.pop("recurrence")
+    task_attributes.pop("recurrence_type")
+    task_attributes["parent_id"] = parent_task.id
+
+    child_task = model.Task(repo.create_next_fulid(model.Task), **task_attributes)
+    child_task.parent = parent_task
+    child_task.tags = parent_task.tags
+    child_task.project = parent_task.project
+
+    repo.add(parent_task)
+    repo.add(child_task)
+    repo.commit()
+    log.info(
+        f"Added {parent_task.recurrence_type} task {parent_task.id}:"
+        f" {parent_task.description}"
+    )
+    log.info(f"Added first child task with id {child_task.id}")
+
+    return parent_task, child_task
+
+
+def set_task_tags(repo: repository.AbstractRepository, task: model.Task) -> None:
+    """
+    Method to set the tags attribute.
+
+    A new tag will be created if it doesn't exist yet.
+    """
+    # commit_necessary = False
+
+    if task.tag_ids is None:
+        return
+
+    for tag_id in task.tag_ids:
+        tag = repo.get(model.Tag, tag_id)
+        if tag is None:
+            tag = model.Tag(id=tag_id, description="")
+            repo.add(tag)
+            log.info(f"Added tag {tag.id}: {tag.description}")
+        elif not isinstance(tag, model.Tag):
+            raise TypeError("Tried to load the wrong object as a tag")
+            # commit_necessary = True
+        task.tags.append(tag)
+
+    # if commit_necessary:
+    #     self.session.commit()
+
+
+def set_task_project(repo: repository.AbstractRepository, task: model.Task):
+    """
+    Function to set the project of a task.
+
+    A new project will be created if it doesn't exist yet.
+    """
+    if task.project_id is not None:
+        project = repo.get(model.Project, task.project_id)
+        if project is None:
+            project = model.Project(id=task.project_id, description="")
+            repo.add(project)
+            log.info(f"Added project {project.id}: {project.description}")
+        elif not isinstance(project, model.Project):
+            raise TypeError("Tried to load the wrong object as a project")
+        task.project = project
 
 
 def install(session, log, data_directory="~/.local/share/pydo"):
-
     """
     Function to create the environment for pydo.
 
@@ -48,6 +152,10 @@ def install(session, log, data_directory="~/.local/share/pydo"):
     log.info("Database initialized")
 
 
+# import json
+# from pydo.model import engine
+
+# from sqlalchemy import MetaData
 # def export(log):
 #     """
 #     Function to export the database to json to stdout.
--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,40 +1,139 @@
-from pydo import model, types
-from pydo.adapters import repository
-from typing import List, Optional
+import logging
+
+import pytest
+
+from pydo import model, services
+from pydo.fulids import fulid
 
 
-class FakeRepository(repository.AbstractRepository):
-    def __init__(
-        self,
-        projects: Optional[List[model.Project]] = None,
-        tags: Optional[List[model.Tag]] = None,
-        tasks: Optional[List[model.Task]] = None,
+class TestTaskAdd:
+    def test_add_task(self, config, repo, faker, caplog):
+        task_attributes = {"description": faker.sentence()}
+
+        task = services.add_task(repo, task_attributes)
+
+        # Ensure that the object is in the repository
+        task = repo.get(model.Task, task.id)
+
+        assert task.description == task_attributes["description"]
+        assert task.state == "open"
+        assert task.project_id is None
+        assert (
+            "pydo.services",
+            logging.INFO,
+            f"Added task {task.id}: {task.description}",
+        ) in caplog.record_tuples
+
+    def test_add_task_generates_secuential_fulid_for_tasks(
+        self, repo, config, faker, insert_task
     ):
-        self._projects = self._initiate_objects(projects)
-        self._tags = self._initiate_objects(tags)
-        self._tasks = self._initiate_objects(tasks)
+        existent_task = insert_task
+        existent_task_fulid_id_number = fulid()._decode_id(existent_task.id)
 
-    def _initiate_objects(self, objects: types.OptionalEntities = None):
-        if objects is None:
-            return set()
-        else:
-            return set(objects)
+        first_task_attributes = {"description": faker.sentence()}
+        first_task = services.add_task(repo, first_task_attributes)
+        first_task_fulid_id_number = fulid()._decode_id(first_task.id)
 
-    def add(self, entity: types.Entity):
-        if isinstance(entity, model.Project):
-            self._projects.add(entity)
-        elif isinstance(entity, model.Tag):
-            self._tags.add(entity)
-        elif isinstance(entity, model.Task):
-            self._tasks.add(entity)
+        second_task_attributes = {"description": faker.sentence()}
+        second_task = services.add_task(repo, second_task_attributes)
+        second_task_fulid_id_number = fulid()._decode_id(second_task.id)
 
-    def _get_object(self, id: str, entities: types.Entities):
-        return next(entity for entity in entities if entity.id == id)
+        assert first_task_fulid_id_number - existent_task_fulid_id_number == 1
+        assert second_task_fulid_id_number - first_task_fulid_id_number == 1
 
-    def get(self, obj_model: types.Entity, id: str) -> types.Entity:
-        if isinstance(obj_model, model.Project):
-            return self._get_object(id, self._projects)
-        elif isinstance(obj_model, model.Tag):
-            return self._get_object(id, self._tags)
-        elif isinstance(obj_model, model.Task):
-            return self._get_object(id, self._tasks)
+    def test_add_task_assigns_project_if_exist(
+        self, config, repo, faker, insert_project
+    ):
+        project = insert_project
+        task_attributes = {"description": faker.sentence(), "project_id": project.id}
+
+        task = services.add_task(repo, task_attributes)
+
+        assert task.project is project
+
+    def test_add_task_generates_project_if_doesnt_exist(
+        self, config, repo, faker, caplog
+    ):
+        task_attributes = {
+            "description": faker.sentence(),
+            "project_id": "non_existent",
+        }
+
+        task = services.add_task(repo, task_attributes)
+        project = repo.get(model.Project, "non_existent")
+
+        assert task.project_id is project.id
+        assert task.project == project
+        assert project.id == "non_existent"
+        assert (
+            "pydo.services",
+            logging.INFO,
+            f"Added project {project.id}: {project.description}",
+        ) in caplog.record_tuples
+
+    def test_add_task_assigns_tag_if_exist(self, config, repo, faker, insert_tag):
+        existent_tag = insert_tag
+        task_attributes = {
+            "description": faker.sentence(),
+            "tag_ids": [existent_tag.id],
+        }
+
+        task = services.add_task(repo, task_attributes)
+
+        assert task.tags == [existent_tag]
+
+    def test_add_task_generates_tag_if_doesnt_exist(self, config, repo, faker, caplog):
+        task_attributes = {
+            "description": faker.sentence(),
+            "tag_ids": ["non_existent"],
+        }
+
+        task = services.add_task(repo, task_attributes)
+        tag = repo.get(model.Tag, "non_existent")
+
+        assert task.tags == [tag]
+        assert tag.id == "non_existent"
+        assert (
+            "pydo.services",
+            logging.INFO,
+            f"Added tag {tag.id}: {tag.description}",
+        ) in caplog.record_tuples
+
+
+@pytest.mark.parametrize("recurrence_type", ["recurring", "repeating"])
+class TestRecurrentTask:
+    def test_add_generates_recurrent_tasks(
+        self, config, repo, faker, caplog, recurrence_type
+    ):
+        task_attributes = {
+            "description": faker.sentence(),
+            "due": faker.date_time(),
+            "recurrence": "1d",
+            "recurrence_type": recurrence_type,
+        }
+
+        parent_task, child_task = services.add_recurrent_task(
+            repo, task_attributes.copy()
+        )
+
+        parent_task = repo.get(model.Task, parent_task.id)
+        child_task = repo.get(model.Task, child_task.id)
+
+        assert child_task.id != parent_task.id
+        assert child_task.parent_id == parent_task.id
+        assert child_task.parent is parent_task
+        assert parent_task.recurrence == task_attributes["recurrence"]
+        assert parent_task.recurrence_type == task_attributes["recurrence_type"]
+        assert parent_task.due == task_attributes["due"]
+        assert child_task.due == task_attributes["due"]
+        assert (
+            "pydo.services",
+            logging.INFO,
+            f"Added {parent_task.recurrence_type} task {parent_task.id}:"
+            f" {parent_task.description}",
+        ) in caplog.record_tuples
+        assert (
+            "pydo.services",
+            logging.INFO,
+            f"Added first child task with id {child_task.id}",
+        ) in caplog.record_tuples
